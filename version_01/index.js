8aee6a32036bbe62c3c56d711ad31eb4aecece35
salinpaul valooran <salinpv@hotmail.com>
2023-11-15 23:55:25 +0530

    showing images when keyword is not blank

--- version_01/index.js
+++ version_01/index.js
@@ -14,36 +14,35 @@ const imagePerPage = "12";
 async function searchImages() {
   keyword = searchBox.value;
   if (keyword !== "") {
-    console.log("hi");
+    const url =
+      baseUrl +
+      page +
+      queryUrl +
+      keyword +
+      apiKeyUrl +
+      imagePerPageUrl +
+      imagePerPage;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (page === 1) {
+      searchResult.innerHTML = "";
+    }
+    const results = data.results;
+
+    results.map((result) => {
+      const image = document.createElement("img");
+      image.src = result.urls.small;
+      const imageLink = document.createElement("a");
+      imageLink.href = result.links.html;
+      imageLink.target = "_blank";
+
+      imageLink.appendChild(image);
+      searchResult.appendChild(imageLink);
+    });
+    showMoreBtn.style.display = "block";
   }
-  const url =
-    baseUrl +
-    page +
-    queryUrl +
-    keyword +
-    apiKeyUrl +
-    imagePerPageUrl +
-    imagePerPage;
-
-  const response = await fetch(url);
-  const data = await response.json();
-
-  if (page === 1) {
-    searchResult.innerHTML = "";
-  }
-  const results = data.results;
-
-  results.map((result) => {
-    const image = document.createElement("img");
-    image.src = result.urls.small;
-    const imageLink = document.createElement("a");
-    imageLink.href = result.links.html;
-    imageLink.target = "_blank";
-
-    imageLink.appendChild(image);
-    searchResult.appendChild(imageLink);
-  });
-  showMoreBtn.style.display = "block";
 }
 
 searchForm.addEventListener("submit", (e) => {
