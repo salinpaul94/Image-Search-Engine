0e50cee3d1764322ccccd333ba3911af1d9e249d
salinpaul valooran <salinpv@hotmail.com>
2023-11-15 22:18:26 +0530

    const for apiKeyUrl

--- version_01/index.js
+++ version_01/index.js
@@ -6,7 +6,7 @@ const searchMoreBtn = document.getElementById("show-more-btn");
 let keyword = "";
 let page = 1;
 const url = "https://api.unsplash.com/search/photos?page=1&query=";
-const apiKey = "8PRX7SZs2ccESPfdmY_dJI831Wa0DOqVaHnuMWFcyXA";
+const apiKeyUrl = "&client_id=8PRX7SZs2ccESPfdmY_dJI831Wa0DOqVaHnuMWFcyXA";
 
 async function searchImages() {
   keyword = searchBox.value;
