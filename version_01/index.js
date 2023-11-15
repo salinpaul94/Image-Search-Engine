b117b5a0823034a708fb0e6a75f8ecb3a2b749fb
salinpaul valooran <salinpv@hotmail.com>
2023-11-15 22:14:00 +0530

    variable for keyword

--- version_01/index.js
+++ version_01/index.js
@@ -2,3 +2,5 @@ const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
 const searchMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
