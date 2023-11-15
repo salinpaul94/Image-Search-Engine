17d4af661a3d8b3d6636db6d884ab7ceb24d1526
salinpaul valooran <salinpv@hotmail.com>
2023-11-15 22:20:28 +0530

    dynamic url for requsting unsplash

--- version_01/index.js
+++ version_01/index.js
@@ -5,9 +5,10 @@ const searchMoreBtn = document.getElementById("show-more-btn");
 
 let keyword = "";
 let page = 1;
-const url = "https://api.unsplash.com/search/photos?page=1&query=";
+const baseUrl = "https://api.unsplash.com/search/photos?page=1&query=";
 const apiKeyUrl = "&client_id=8PRX7SZs2ccESPfdmY_dJI831Wa0DOqVaHnuMWFcyXA";
 
 async function searchImages() {
   keyword = searchBox.value;
+  const url = baseUrl + keyword + apiKeyUrl;
 }
